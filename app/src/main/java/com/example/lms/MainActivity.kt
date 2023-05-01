0e0caa74ff30aa405bf1561331bb00e0df6ae9ee
Shruthaja <mail.shruthaja@gmail.com>
2023-05-01 12:54:27 -0500

    Hari om

---
+++ app/src/main/java/com/example/lms/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.lms
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.color.DynamicColors
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+System.out.println("hey");
+    }
+}
+
