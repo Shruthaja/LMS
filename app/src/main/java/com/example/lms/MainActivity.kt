a4df7c97d8b397e354f9aecac890332b73504441
Shruthaja <mail.shruthaja@gmail.com>
2023-05-02 00:11:31 -0500

    with redirection to the pages.

--- app/src/main/java/com/example/lms/MainActivity.kt
+++ app/src/main/java/com/example/lms/MainActivity.kt
@@ -12,6 +12,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.StringBuilder
 import org.json.*
+import java.net.URLEncoder
 import org.json.JSONTokener
 
 class MainActivity : AppCompatActivity() {
@@ -22,19 +23,29 @@ class MainActivity : AppCompatActivity() {
 
         StrictMode.setThreadPolicy(policy)
         // get reference to button
-        val btnclickme = findViewById(R.id.button3) as Button
+        val btnclickme = findViewById(R.id.button) as Button
         // set on-click listener
         btnclickme.setOnClickListener {
             val value=findViewById(R.id.textView6) as TextView
             try {
                 // Create URL object
-                val url = URL("http://172.22.80.1/hello.php/resource")
 
-                // Create HTTP connection
+
+// Set the request body
+                val baseUrl = "http://172.22.80.1/hello.php"
+                val bname = "To Kill a Mockingbird"
+                val cardno="123456"
+                val encodedBname = URLEncoder.encode(bname, "UTF-8")
+val encodedcardno=URLEncoder.encode(cardno,"UTF-8")
+                val urlString = "$baseUrl?bookname=$encodedBname&cardno=$encodedBname"
+System.out.println(urlString);
+                val url = URL(urlString)
+
+// Create HTTP connection
                 val connection = url.openConnection() as HttpURLConnection
                 connection.requestMethod = "GET"
 
-                // Read response
+// Read response
                 val inStream = BufferedReader(InputStreamReader(connection.inputStream))
                 val response = StringBuilder()
                 var inputLine: String?
@@ -42,24 +53,92 @@ class MainActivity : AppCompatActivity() {
                     response.append(inputLine)
                 }
                 inStream.close()
-                val jsonObject = JSONTokener(response.toString()).nextValue() as JSONObject
-                val jsonArray = jsonObject.getJSONArray("data")
-                val tableBuilder = StringBuilder()
 
-                // Print response
-//                value.setText(response.toString())
-                for (i in 0 until jsonArray.length()) {
-                    val item = jsonArray.getJSONObject(i)
+                val jsonResponse = (response.toString())
 
-                }
-                value.setText(jsonObject.getJSONObject("title").toString())
+                value.setText(jsonResponse)
 
             } catch (e: Exception) {
                 e.printStackTrace()}
 //            value.setText("Result")
 
         }
+        val btnclickme2 = findViewById(R.id.button2) as Button
+        btnclickme2.setOnClickListener{
+            val value=findViewById(R.id.textView6) as TextView
+            try {
+                // Create URL object
 
+
+// Set the request body
+                val baseUrl = "http://172.22.80.1/newbook.php"
+                val newbname = "To Kill a Mockingbird"
+                val publisher="ABC"
+                val encodednewbname = URLEncoder.encode(newbname, "UTF-8")
+                val encodedpublisher=URLEncoder.encode(publisher,"UTF-8")
+                val urlString = "$baseUrl?bookname=$encodednewbname&publisher=$encodedpublisher"
+                System.out.println(urlString);
+                val url = URL(urlString)
+
+// Create HTTP connection
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+
+// Read response
+                val inStream = BufferedReader(InputStreamReader(connection.inputStream))
+                val response = StringBuilder()
+                var inputLine: String?
+                while (inStream.readLine().also { inputLine = it } != null) {
+                    response.append(inputLine)
+                }
+                inStream.close()
+
+                val jsonResponse = (response.toString())
+
+                value.setText(jsonResponse+"New value!")
+
+            } catch (e: Exception) {
+                e.printStackTrace()}
+
+        }
+        val btnclickme3=findViewById(R.id.button3) as Button
+        btnclickme3.setOnClickListener{
+            val value=findViewById(R.id.textView6) as TextView
+            try {
+                // Create URL object
+
+
+// Set the request body
+                val baseUrl = "http://172.22.80.1/checkdue.php"
+                val sdate = "abc"
+                val edate="ABC"
+                val encodedsdate = URLEncoder.encode(sdate, "UTF-8")
+                val encodededate=URLEncoder.encode(edate,"UTF-8")
+                val urlString = "$baseUrl?sdate=$encodedsdate&edate=$encodededate"
+                System.out.println(urlString);
+                val url = URL(urlString)
+
+// Create HTTP connection
+                val connection = url.openConnection() as HttpURLConnection
+                connection.requestMethod = "GET"
+
+// Read response
+                val inStream = BufferedReader(InputStreamReader(connection.inputStream))
+                val response = StringBuilder()
+                var inputLine: String?
+                while (inStream.readLine().also { inputLine = it } != null) {
+                    response.append(inputLine)
+                }
+                inStream.close()
+
+                val jsonResponse = (response.toString())
+
+                value.setText(jsonResponse+"Date value!")
+
+            } catch (e: Exception) {
+                e.printStackTrace()}
+
+        }
         val switchToSecondActivity=findViewById(R.id.button4) as Button
         switchToSecondActivity.setOnClickListener({
             switchActivities()
